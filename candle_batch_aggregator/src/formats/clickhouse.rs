b7c0dcad97be7ddb3ade14054023b25904e59e02
Eugen Soloviov <suenot@gmail.com>
2025-05-15 00:14:05 +0300

    feat: Implement ClickHouse batch aggregation stub

--- candle_batch_aggregator/src/formats/clickhouse.rs
+++ candle_batch_aggregator/src/formats/clickhouse.rs
@@ -1,1 +1,18 @@
- 
+// Заготовка для batch-агрегации ClickHouse
+
+// TODO: Импортировать необходимые crates (clickhouse, polars, и т.д.)
+// use polars::prelude::*;
+// use clickhouse::Client;
+
+pub struct ClickhouseTrade {
+    // TODO: определить структуру трейда для ClickHouse
+}
+
+pub fn read_trades_from_clickhouse(_conn_str: &str) -> Vec<ClickhouseTrade> {
+    // TODO: реализовать чтение трейдов из ClickHouse
+    vec![]
+}
+
+pub fn write_candles_to_clickhouse(_conn_str: &str, _candles: &[/*Candle*/]) {
+    // TODO: реализовать запись свечей в ClickHouse
+} 
