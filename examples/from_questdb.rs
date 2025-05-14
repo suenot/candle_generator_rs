99ec68b2268537bcee5eb3f8c8a2f9b631badba6
Eugen Soloviov <suenot@gmail.com>
2025-05-14 23:26:35 +0300

    refactor: Import chrono TimeZone trait in from_questdb example

--- examples/from_questdb.rs
+++ examples/from_questdb.rs
@@ -1,5 +1,6 @@
 use candle_generator::{CandleGenerator, Timeframe, Trade, Instrument, Pair, MarketType, Side};
 use chrono::Utc;
+use chrono::TimeZone;
 use reqwest::blocking::get;
 use csv::ReaderBuilder;
 
