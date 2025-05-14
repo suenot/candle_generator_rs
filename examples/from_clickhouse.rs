bf2369d94307b04562946123cec2a200617733fc
Eugen Soloviov <suenot@gmail.com>
2025-05-14 23:26:30 +0300

    refactor: Update chrono imports in from_clickhouse example

--- examples/from_clickhouse.rs
+++ examples/from_clickhouse.rs
@@ -1,5 +1,5 @@
 use candle_generator::{CandleGenerator, Timeframe, Trade, Instrument, Pair, MarketType, Side};
-use chrono::Utc;
+use chrono::{Utc, TimeZone};
 use reqwest::blocking::Client;
 use csv::ReaderBuilder;
 
