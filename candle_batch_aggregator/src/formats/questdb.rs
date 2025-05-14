a1e1eee2d88517e32a016ea166ee9a3edc01920a
Eugen Soloviov <suenot@gmail.com>
2025-05-15 00:13:59 +0300

    feat: Implement initial QuestDB batch aggregation functionality

--- candle_batch_aggregator/src/formats/questdb.rs
+++ candle_batch_aggregator/src/formats/questdb.rs
@@ -1,1 +1,18 @@
- 
+// Заготовка для batch-агрегации QuestDB
+
+// TODO: Импортировать необходимые crates (questdb, polars, и т.д.)
+// use polars::prelude::*;
+// use questdb::Client;
+
+pub struct QuestdbTrade {
+    // TODO: определить структуру трейда для QuestDB
+}
+
+pub fn read_trades_from_questdb(_conn_str: &str) -> Vec<QuestdbTrade> {
+    // TODO: реализовать чтение трейдов из QuestDB
+    vec![]
+}
+
+pub fn write_candles_to_questdb(_conn_str: &str, _candles: &[/*Candle*/]) {
+    // TODO: реализовать запись свечей в QuestDB
+} 
