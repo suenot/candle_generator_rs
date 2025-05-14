6c140923fd3476ae7e77ca8eb66ac3f24086f961
Eugen Soloviov <suenot@gmail.com>
2025-05-14 23:58:15 +0300

    feat: Implement CSV batch processing stub function

--- candle_batch_aggregator/src/formats/csv.rs
+++ candle_batch_aggregator/src/formats/csv.rs
@@ -1,1 +1,9 @@
- 
+use super::super::Args;
+use anyhow::Result;
+
+pub fn process_csv_batch(args: &Args) -> Result<()> {
+    println!("[CSV] Batch processing: input={:?}, output={:?}, symbol={}, interval={}, progress={}, benchmark={}",
+        args.input, args.output, args.symbol, args.interval, args.progress, args.benchmark);
+    // TODO: реализовать чтение трейдов, агрегацию, запись свечей
+    Ok(())
+} 
